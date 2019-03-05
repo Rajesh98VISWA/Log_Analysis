8e84f18cfd58d6e5e1ee28518818ba62934d023b
Rajesh98VISWA <viswanadharajesh@gmail.com>
2019-03-05 10:37:08 +0530

    this is it

--- news.py
+++ news.py
@@ -1,66 +1,81 @@
 #!/usr/bin/env python3
 import psycopg2
 
-from datetime import datetime
+# Answer1
+PopularArticles = """
+SELECT articles.title,
+count(*)
+FROM log,articles
+WHERE log.path = '/article/' || articles.slug
+GROUP BY articles.title
+ORDER BY count(*) DESC
+LIMIT 3;
+"""
+# Answer2
+PopularAuthors = """
+SELECT authors.name,
+count(*)
+FROM log,articles,authors
+WHERE log.path = '/article/' || articles.slug
+GROUP BY authors.name
+ORDER BY count(*) DESC;
+"""
 
-query_1 = ("SELECT a.title, count(*) AS views "
-           "FROM articles a INNER JOIN log b "
-           "on a.slug=replace(path,'/article/','') "
-           "WHERE status='200 OK' AND length(path)>1 GROUP by "
-           "a.title ORDER by views DESC limit 3")
-
-query_2 = ("SELECT c.name, count(*) AS views "
-           "FROM articles a INNER JOIN log b "
-           "on a.slug=replace(path,'/article/','') INNER JOIN "
-           "authors c on (c.id=a.author) "
-           "WHERE status='200 OK' AND length(path)>1 GROUP by "
-           "c.name ORDER by views DESC")
-
-query_3 = """WITH num_requests AS (
-	SELECT time::date AS day, count(*)
-	FROM log
-	GROUP BY time::date
-	ORDER BY time::date
-	), num_errors AS (
-	SELECT time::date AS day,count(*)
-	FROM log
-	WHERE status != '200 OK'
-	GROUP BY time::date
-	ORDER BY time::date
-	), error_rate AS (
-	SELECT num_requests.day ,
-	 num_errors.count::float /num_requests.count::float *100
-	 AS error_pc
-	FROM num_requests,num_errors
-	WHERE num_requests.day = num_errors.day
-	)
-	SELECT * FROM error_rate WHERE error_pc > 1;
-	"""
+# Answer3
+MoreThanOnePercent = """
+WITH num_requests AS (
+SELECT Time::Date AS day, count(*)
+FROM log
+GROUP BY Time::Date
+ORDER BY Time::Date
+), num_errors AS (
+SELECT Time::Date AS day,count(*)
+FROM log 
+WHERE status != '200 OK'
+GROUP BY Time::Date
+ORDER BY Time::Date
+), error_rate AS (
+SELECT num_requests.day ,
+num_errors.count::float /num_requests.count::float *100
+AS error_pc
+FROM num_requests,num_errors
+WHERE num_requests.day = num_errors.day
+)
+SELECT * FROM error_rate WHERE error_pc > 1;
+"""
 
 
-def f():
-    d = psycopg2.connect("dbname=news")
-    cur = d.cursor()
-    cur.execute(query_1)
-    results = cur.fetchall()
-    print("Most Popular Articles")
-    for result in results:
-        print('"{title}" - {count} views'.format(title=result[0], count=result[1])+ "\n")
+def Writer_checker():
+    c = psycopg2.connect("dbname=news")
+    blank = c.cursor()
+### Popular Articles  execution
 
-    cur.execute(query_2)
-    results = cur.fetchall()
-    print("Most Popular authors")
-    for result in results:
-        print('{author} - {count} views'.format(
-            author=result[0], count=result[1])+"\n")
+    blank.execute(PopularArticles)
+    results = blank.fetchall()
+    print("\n***Most--Popular--Articles***\n")
+    for i in results:
+        print(
+            '"{title}"" - {count} views'.format(title=i[0], count=i[1]) 
+            )
+### Popular Authors execution
 
-    cur.execute(query_3)
-    results = cur.fetchall()
-    print("Days in Which More Than One Percent of Error")
-    for result in results:
-    	print('{date:%B %d, %Y}-{error_rate:.1f}% errors'.format(date=result[0],error_rate=result[1]))
-    	cur.close()
-    	d.close()
+    blank.execute(PopularAuthors)
+    results = blank.fetchall()
+    print("\n***Most--Popular--Authors***\n")
+    for i in results:
+        print('{author} - {count} views'.format(author=i[0], count=i[1]))
+
+### Days in which more than one percent error execution
+
+    blank.execute(MoreThanOnePercent)
+    results = blank.fetchall()
+    print("\n***--Days in Which More Than One Percent Error--***\n")
+    for i in results:
+        print(
+            '{date:%B %d, %Y}-{error_rate:.1f}% errors'.
+            format(date=i[0], error_rate=i[1]))
+    blank.close()
+    c.close()
 
 if __name__ == '__main__':
-    f()
+    Writer_checker()
